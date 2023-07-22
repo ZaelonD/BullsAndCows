1f18465b127634e21152e1bbe2c03a52b9200baf
Alexander Dubrovin <126788653+ZaelonD@users.noreply.github.com>
2023-07-22 17:22:32 +0300

    Update BullsAndCowsRunner.java
    Correcting grammatical errors

--- src/main/java/BullsAndCowsRunner.java
+++ src/main/java/BullsAndCowsRunner.java
@@ -1,6 +1,6 @@
 public class BullsAndCowsRunner {
     public static void main(String[] args) {
-        // Выбираем имплиментацию игры, когда компьютер загадывает строку
+        // Выбираем имплементацию игры, когда компьютер загадывает строку
         GameLogic gameLogic = new ComputerMakesStringGameLogicImpl();
         // Запускаем игру
         gameLogic.play();
